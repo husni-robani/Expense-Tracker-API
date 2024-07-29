a7b0b4efefdd5159345fa78bcd92354981f482dd
husni-robani <husnir2005@gmail.com>
2024-07-29 14:56:33 +0700

    create signup and signin dto for authentication

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class SigninDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
