1a0ac23599c54d8fa534162e9159aa426c039043
husni-robani <husnir2005@gmail.com>
2024-07-30 08:15:53 +0700

    setup jwt token for authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      global: true,
+    }),
+  ],
   controllers: [AuthController],
-  imports: [PrismaModule],
   providers: [AuthService],
 })
 export class AuthModule {}
